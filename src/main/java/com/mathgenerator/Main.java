864e8a962fbd20b8b3d90f0e437f133c0a7a29c6
zg <2679484298@qq.com>
2025-03-13 17:57:07 +0800

    <fit> 修正标准化方法的bug引起的问题

--- src/main/java/com/mathgenerator/Main.java
+++ src/main/java/com/mathgenerator/Main.java
@@ -3,9 +3,13 @@ package com.mathgenerator;
 import com.mathgenerator.handler.FileHandler;
 import com.mathgenerator.generator.ExpressionGenerator;
 import com.mathgenerator.generator.ExpressionGenerator.ExpressionResult;
+import com.mathgenerator.model.Fraction;
+import com.mathgenerator.service.ExpressionNormalizer;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,7 +19,7 @@ import java.util.Set;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        if (!(args.length == 6 || args.length ==8)) {
+        if (!(args.length == 6 || args.length == 8)) {
             System.out.println("Usage: Myapp.exe -n <number> -r <range> -e <exercisefile>.txt -a <answerfile>.txt");
             System.out.println("   or: Myapp.exe -e <exercisefile>.txt -a <answerfile>.txt -g <gradefile>.txt");
             return;
@@ -34,19 +38,21 @@ public class Main {
                 System.out.println("Range parameter -r is required and must be between 1 and 10 (not including 10).");
                 return;
             }
-
             ExpressionGenerator generator = new ExpressionGenerator(r);
-            Set<String> expressions = new HashSet<>();
+            ExpressionNormalizer normalizer = new ExpressionNormalizer();
+            Map<String, Fraction> expressions = new HashMap<>();
             while (expressions.size() < n) {
                 ExpressionResult result = generator.generateExpression();
-                String normalizedExpression = generator.normalizeExpression(result.getExpression());
-                expressions.add(normalizedExpression);
+               // String normalizedExpression = result.getExpression();
+                String normalizedExpression = normalizer.normalizeExpression(result.getExpression());
+                if (expressions.containsKey(normalizedExpression)) {
+                    continue;
+                }
+                expressions.put(normalizedExpression, result.getResult());
             }
-
             // 将题目和答案写入文件
             FileHandler.writeExpressions(expressions, args[5]);
             FileHandler.writeAnswers(expressions, args[7]);
-
             System.out.println("生成题目和答案成功！\n题目已写入" + args[5] + "\n答案已写入" + args[7]);
         }
         // 处理对比答案的逻辑
---
+++ src/test/java/GradeTest.java
@@ -0,0 +1,25 @@
+import com.mathgenerator.generator.ExpressionGenerator;
+import com.mathgenerator.handler.FileHandler;
+import com.mathgenerator.model.Fraction;
+import com.mathgenerator.service.ExpressionEvaluator;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+/**
+ * @author: YiLaiL
+ * @date: 2025/03/12
+ * @description: 测试比较答案函数
+ */
+public class GradeTest {
+    @Test
+    public void testGradeAnswers() {
+//        Fraction actualResult = ExpressionEvaluator.evaluate();
+//        Fraction expectedResult = ExpressionEvaluator.evaluate(expression);
+//        if (expectedResult.equals(actualResult)) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+    }
+}
